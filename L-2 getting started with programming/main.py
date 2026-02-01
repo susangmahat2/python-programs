73296f834f829b14dfd594d8df690b71c57b87af
susangmahat2 <susangmahat2@gmail.com>
2026-02-01 20:46:05 +0900

    initial commit

---
+++ L-2 getting started with programming/main.py
@@ -0,0 +1,10 @@
+print ("module 1 Lesson-2")
+print("Welcome to the world of programming!")
+print(4)
+print("Hello Python \n")
+
+#Pass multiple arguments for printing
+print("hello",5)
+
+#End argument of Print Statement
+print("wlcome to",end="*")

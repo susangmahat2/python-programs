588b912fddb4d34f8591de80e0e3f92b2e4758ef
susangmahat2 <susangmahat2@gmail.com>
2026-02-06 21:01:21 +0900

    3rd commit

---
+++ L=3 Data types/activity3.py
@@ -0,0 +1,10 @@
+variable1=7
+print("type of variable1",type(variable1))
+Table=False
+print("type of Table",type(Table))
+Spoon=7.6666
+print("type of Spoon",type(Spoon))
+Brother="susang"
+print("type of Brother",type(Brother))
+slice=Brother[0:3:2]
+print(slice)

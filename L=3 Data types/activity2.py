593f60870bd8e86dfb029eac737a111512c39193
susangmahat2 <susangmahat2@gmail.com>
2026-02-06 20:49:54 +0900

    2nd commit

---
+++ L=3 Data types/activity2.py
@@ -0,0 +1,11 @@
+variable1=7
+print("type of variable1",type(variable1))
+Table=False
+print("type of Table",type(Table))
+Spoon=7.6666
+print("type of Spoon",type(Spoon))
+Brother="susang"
+print("type of Brother",type(Brother))
+convert=float(variable1)
+print(convert)
+print(type(convert))

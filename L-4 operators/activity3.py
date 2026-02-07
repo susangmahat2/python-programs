cb846f84bdaf501c33f224f65a44feec84610d88
susangmahat2 <susangmahat2@gmail.com>
2026-02-07 21:06:27 +0900

    3rd commit

---
+++ L-4 operators/activity3.py
@@ -0,0 +1,7 @@
+math=int(input("Enter your math marks :"))
+english=int(input("Enter your english marks :"))
+science=int(input("Enter your science marks :"))
+hindi=int(input("Enter your hindi marks :"))
+sum=math+english+science+hindi
+percentage=(sum/400)*100
+print("the percentage of all the subject",percentage)

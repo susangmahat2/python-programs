9d42a5a50ce2714e31f4026bfb82e3fa04177c83
susangmahat2 <susangmahat2@gmail.com>
2026-02-13 20:49:52 +0900

    initiql commit

---
+++ l=6 python opretersII/activity1.py
@@ -0,0 +1,18 @@
+a=8
+b=-8
+c=0
+if a and b and c:
+    print("All the numbers have boolean value True")
+else:
+    print("atleast one of the numbers has boolean value ")
+a=9
+b=-9
+c=0
+if a>0 or b>0:
+    print("enither of the number is greater than zero")
+else:
+    print("No number is greater than zero")
+if b>0 or c>0:
+    print("enither of the number is greater than zero")
+else:
+    print("No number is greater than zero")

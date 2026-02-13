311d0a55481eb2ea969f842f7a24ed7a39977776
susangmahat2 <susangmahat2@gmail.com>
2026-02-13 21:00:13 +0900

    2nd commit

---
+++ l=6 python opretersII/activity2.py
@@ -0,0 +1,19 @@
+a=5
+b=12
+c=12
+print(a!=b)
+print(b!=c)
+
+a="coding"
+b="python"
+if a!=b:
+    print(a,"and",b,"are not equal")
+a=2
+b=3
+if (a==1)!=(b==3):
+    print("Condition is True")
+
+a=int(input("Enter a number: "))
+if a%2!=0:
+    print(a,"is an odd number")
+    

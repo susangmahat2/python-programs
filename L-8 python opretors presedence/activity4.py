ee71e6ac79312bb1998ef3a84153cee253657e1c
susangmahat2 <susangmahat2@gmail.com>
2026-02-15 21:10:30 +0900

    4th commit

---
+++ L-8 python opretors presedence/activity4.py
@@ -0,0 +1,11 @@
+speed1=10
+speed2=20
+speed3=30 
+avg=(speed1+speed2+speed3)/3
+if speed1>avg:
+    print("speed1 is above average")
+if speed2>avg:
+    print("speed2 is above average")
+if speed3>avg:
+    print("speed3 %d is above average %d" % (speed3, avg))
+

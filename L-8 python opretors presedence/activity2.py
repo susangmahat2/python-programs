fa1c6a0d3f69ab6849545f343bf04000ab497caf
susangmahat2 <susangmahat2@gmail.com>
2026-02-15 20:42:34 +0900

    2nd commit

---
+++ L-8 python opretors presedence/activity2.py
@@ -0,0 +1,7 @@
+num1=int(input("Enter the first number (numerator): "))
+num2=int(input("Enter the second number(denomenetor): "))
+if num1%num2==0:
+    print("\n" +str(num1)+ " is divisible by " +str(num2))
+else:
+
+    print("\n" +str(num1)+ " is not divisible by " +str(num2))
